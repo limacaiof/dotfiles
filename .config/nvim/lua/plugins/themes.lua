d845eabeb8f20039f0555708eb1aad93e121023b
limacaio <dev.caiolima@gmail.com>
2025-01-15 10:25:52 -0300

    change nvim theme bg color

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -6,6 +6,18 @@ return {
         priority = 1000,
         opts = {},
         config = function()
+            require("tokyonight").setup({
+              -- disable italic for functions
+              styles = {
+                functions = {}
+              },
+              -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+              on_colors = function(colors)
+                colors.bg= "#0b0b0b"
+                colors.bg_dark= "#0b0b0b"
+                colors.bg_dark_1= "#0b0b0b"
+              end
+            })
             vim.cmd[[colorscheme tokyonight-night]]
         end
     },

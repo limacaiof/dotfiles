1d0bcfa4e48cbb5c5b36327cc2dc1b023b259728
Caio Lima <limacaiof>
2025-07-22 13:12:13 -0300

    add stuff

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,32 +1,37 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		vim.keymap.set(
-			"n",
-			"<C-n>",
-			":NvimTreeToggle <CR>",
-			{ desc = "Toggle File Tree", noremap = true, silent = true }
-		)
-		require("nvim-tree").setup({
-			view = {
-				width = 35,
-				side = "right",
-			},
-			renderer = {
-				group_empty = false,
-			},
-			filters = {
-				dotfiles = false,
-				custom = { "^.git$" },
-			},
-			git = {
-				ignore = false,
-			},
-		})
-	end,
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        vim.keymap.set(
+            "n",
+            "<C-n>",
+            ":NvimTreeToggle <CR>",
+            { desc = "Toggle File Tree", noremap = true, silent = true }
+        )
+
+        require("nvim-tree").setup({
+            sync_root_with_cwd = true,
+            view = {
+                width = 35,
+                side = "right",
+            },
+            renderer = {
+                group_empty = false,
+                root_folder_label = function(path)
+                    return vim.fn.fnamemodify(path, ":t")
+                end,
+            },
+            filters = {
+                dotfiles = false,
+                custom = { "^.git$" },
+            },
+            git = {
+                ignore = false,
+            },
+        })
+    end,
 }
